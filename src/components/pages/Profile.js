f84b315c1aaa5deb61707dcb48d04e861bdc1f0f
minnunae <youminzawa@gmail.com>
2021-09-23 18:23:30 +0700

    first commit1

---
+++ src/components/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from "react"
+function Profile(){
+    return(
+        <div className="container">
+            <div className="card mt-4">
+                <div className="card-body">
+                    <h2>Profile page</h2>
+                </div>
+            </div> 
+        </div>
+    )
+}
+export default Profile;
